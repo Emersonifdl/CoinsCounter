d78cf5b996483ea9b054029410380f5acc7480b6
Emerson Lima <emerson@dimap.ufrn.br>
2019-06-27 14:19:52 -0300

    modificações

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,7 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Cofre, Transacao
+from .models import Cofre, Transacao, Local
 
+admin.site.register(Local)
 admin.site.register(Cofre)
-
 admin.site.register(Transacao)
